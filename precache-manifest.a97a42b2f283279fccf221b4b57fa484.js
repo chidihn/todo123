0286bf10394f5b63d6ae7d574e1697013d9109f9
Bùi Đình Chí <chi.bui@invigroup.com>
2019-03-25 22:12:16 +0700

    Updates

---
+++ precache-manifest.a97a42b2f283279fccf221b4b57fa484.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0cbd0847616c5b88e858a5e2abacdbe",
+    "url": "/todo123/static/media/tick.c0cbd084.svg"
+  },
+  {
+    "revision": "d29f1779664178a55ed87236fd97c302",
+    "url": "/todo123/static/media/checkdone.d29f1779.svg"
+  },
+  {
+    "revision": "40eb9cc6d51d63d1dfd8a0ea29b8d700",
+    "url": "/todo123/static/media/check.40eb9cc6.svg"
+  },
+  {
+    "revision": "9136ab028c726fd18747",
+    "url": "/todo123/static/js/runtime~main.9136ab02.js"
+  },
+  {
+    "revision": "843e000f8f2641e0d6cb",
+    "url": "/todo123/static/js/main.843e000f.chunk.js"
+  },
+  {
+    "revision": "d1d24e56a3503e07f417",
+    "url": "/todo123/static/js/2.d1d24e56.chunk.js"
+  },
+  {
+    "revision": "843e000f8f2641e0d6cb",
+    "url": "/todo123/static/css/main.9ef51593.chunk.css"
+  },
+  {
+    "revision": "0f48fed536761e1cd25b147459a220b9",
+    "url": "/todo123/index.html"
+  }
+];
